b4b981f39d4098ae9e2a8068a72260edf04a2cc1
Mayank Diwate <MayankDiwate007@gmail.com>
2024-04-03 22:37:38 +0530

    first commit

---
+++ app/_components/Header.jsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between p-5 shadow-md">
+      <div>
+        <Image
+          src="/logo.svg"
+          alt="Logo"
+          width={100}
+          height={100}
+          className="w-[150px] md:w-[200px]"
+        />
+      </div>
+      <ul className="hidden md:flex gap-4 lg:gap-8 font-medium text-lg">
+        <li className="hover:text-primary transition-all duration-300 cursor-pointer">
+          Product
+        </li>
+        <li className="hover:text-primary transition-all duration-300 cursor-pointer">
+          Pricing
+        </li>
+        <li className="hover:text-primary transition-all duration-300 cursor-pointer">
+          Contact Us
+        </li>
+        <li className="hover:text-primary transition-all duration-300 cursor-pointer">
+          About Us
+        </li>
+      </ul>
+      <div className="flex gap-5">
+        <Button variant="ghost" size="icon" className="w-10 h-10">
+          Login
+        </Button>
+
+        <Button>Get Started</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/_components/Hero.jsx
@@ -0,0 +1,76 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center justify-center my-20">
+      <div className="hidden md:block">
+        <Image
+          src="/profile1.png"
+          width={100}
+          height={100}
+          className="h-[100px] z-[-10] object-cover rounded-full absolute right-20"
+          alt="profile"
+        />
+        <Image
+          src="/profile2.png"
+          width={100}
+          height={100}
+          className="h-[100px] z-[-10] object-cover rounded-full absolute top-60 left-24"
+          alt="profile"
+        />
+        <Image
+          src="/profile3.png"
+          width={100}
+          height={100}
+          className="h-[100px] z-[-10] object-cover rounded-full absolute bottom-20 left-36"
+          alt="profile"
+        />
+        <Image
+          src="/profile4.png"
+          width={100}
+          height={100}
+          className="h-[100px] z-[-10] object-cover rounded-full absolute right-16 bottom-32"
+          alt="profile"
+        />
+      </div>
+      <div className="text-center max-w-3xl">
+        <h2 className="font-bold text-[60px] text-slate-700">
+          Easy Scheduling ahead
+        </h2>
+        <h2 className="font-xl mt-5 text-slate-700">
+          Scheduly is your scheduling automation platform for eliminating the
+          back-and-forth emails to find the perfect time -- and so much more
+        </h2>
+        <div className="flex gap-4 flex-col mt-5">
+          <h3>Sign Up free with Google and Facebook</h3>
+          <div className="flex justify-center gap-8 ">
+            <Button className="flex items-center gap-2">
+              <Image src="/google.png" width={25} height={25} alt="google" />
+              <span>Sign Up with Google</span>
+            </Button>
+            <Button className="flex items-center gap-2">
+              <Image
+                src="/facebook.png"
+                width={25}
+                height={25}
+                alt="facebook"
+              />
+              <span>Sign Up with Facebook</span>
+            </Button>
+          </div>
+        </div>
+        <hr className="mt-4 mb-2" />
+        <h3>
+          Already have an account?{" "}
+          <Link href="/login">
+            <span>Log in</span>
+          </Link>
+        </h3>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
